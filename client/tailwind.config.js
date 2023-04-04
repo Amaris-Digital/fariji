1565580c12250c282691705945b21c8d48916283
Ian Okumu <okumu.otsembo@gmail.com>
2023-04-04 22:43:18 +0300

    CLIENT: Setup flowbite

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,9 +3,12 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
