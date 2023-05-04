bea481a167b23959ab5768a434767c416b316da6
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-05-04 18:04:59 +0300

    DATABASE: user model DB schema

---
+++ api/db/migrate/20230504145408_users.rb
@@ -0,0 +1,11 @@
+class Users < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :phone, null: false, unique: true
+      t.string :email, unique: true
+      t.date :date_of_birth
+      t.boolean :culture, default: false
+      t.timestamps
+    end
+  end
+end
