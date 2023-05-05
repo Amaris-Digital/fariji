078609d24595c2c410266a569d10835d14233de0
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-05-05 14:59:18 +0300

    add user dd schem

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    enum :status, { inactive: 0, active: 1 }
+
+    validates :email, uniqueness: true
+    validates :phone, presence: true, length: { minimum: 9, maximum: 12 }
+    validates :password, length: { minimum: 6 }
+    validate :at_least_eighteen_years_old
+
+
+    private
+
+    def at_least_eighteen_years_old
+        if date_of_birth > 18.years.ago.to_date
+            errors.add(:date_of_birth, "You must be at least eighteen years old")
+        end
+    end
+
 end
--- api/db/migrate/20230504174232_create_users.rb
+++ api/db/migrate/20230504174232_create_users.rb
@@ -3,8 +3,10 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :phone, null: false, unique: true
       t.string :email, unique: true
-      t.date :date_of_birth
-      t.boolean :culture, default: false
+      t.date :date_of_birth, null: false
+      t.boolean :isMuslim, default: false
+      t.integer :status, default: 0
+      t.string :password_digest
       t.timestamps
     end
   end
