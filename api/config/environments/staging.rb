6402f710696bbac2648a7bc9f2a849e02dafdc03
otsembo <okumu.otsembo@gmail.com>
2023-05-08 12:51:28 +0300

    Add static files serving

--- api/config/environments/staging.rb
+++ api/config/environments/staging.rb
@@ -30,6 +30,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
