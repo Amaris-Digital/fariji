e7e39aa5a7447b3257476288fedd1c52c2cf4287
emmanuelkaringi <emmanuelkaringi@gmail.com>
2023-05-09 06:18:45 +0300

    Login: add user login mutation

---
+++ api/app/graphql/types/payloads/login_user_type.rb
@@ -0,0 +1,8 @@
+module Types
+    module Payloads
+        class LoginUserType < Types::BaseObject
+            field :token, String, null: false
+            field :user, Types::UserType, null: false
+        end
+    end
+end
