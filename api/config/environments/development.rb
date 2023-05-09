5d658d48c8e036b2c21b2662db37e112e29064d6
otsembo <okumu.otsembo@gmail.com>
2023-05-09 07:22:00 +0300

    change file server enabled

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -53,6 +53,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
