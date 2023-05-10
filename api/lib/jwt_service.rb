a727c97a9f0725e398b6f6537e5664d06ad48d98
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-05-10 14:55:51 +0300

    Corrected linters errors

--- api/lib/jwt_service.rb
+++ api/lib/jwt_service.rb
@@ -1,33 +1,29 @@
-require "jwt"
+# frozen_string_literal: true
 
+require 'jwt'
 
+# JWTService
 module TokenAuthentication
-    # encoding data into a jwt token
+  # encoding data into a jwt token
 
-    def self.encode(payload, exp = 24.hours.from_now)
-        payload[:exp] = exp.to_i
-        JWT.encode(payload, Rails.application.secrets.secret_key_base)
-    end
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
 
-    # decoding a jwt token
-    def self.decode(token)
-        JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+  # decoding a jwt token
+  def self.decode(token)
+    JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+  rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+    nil
+  end
 
-    rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
-        nil
-    end
+  # refreshing a jwt token
+  def self.refresh(token)
+    decoded = decode(token)
+    return unless decoded && decoded[:exp] > Time.now.to_i
 
-    #refreshing a jwt token
-    def self.refresh(token)
-        decoded = decode(token)
-        if decoded && decoded[:exp] > Time.now.to_i
-            payload = decoded.reject { |key, value| key == 'exp' }
-            encode(payload, Time.now.to_i + 24.hours.to_i)
-        else
-            nil
-        end
-      end
-    end
-
-    
+    payload = decoded.except('exp')
+    encode(payload, Time.now.to_i + 24.hours.to_i)
+  end
 end
