75d0e4f1107d8adf33f67d342b50b6daaee46c52
chelahcynthia <cchelangat989@gmail.com>
2023-05-12 13:45:11 +0300

    Add user type objects and mutation

--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -3,10 +3,28 @@
 module Types
   class MutationType < Types::BaseObject
     # TODO: remove me
-    field :test_field, String, null: false,
-                               description: 'An example field added by the generator'
-    def test_field
-      'Hello World'
+    field :register, Types::UserType, null: false do
+      argument :phone, String, required: true
+      argument  :email, String, required:false
+      argument :date_of_birth, GraphQL::Types::ISO8601DateTime, required: true
+      argument :isMuslim, Boolean, required: false
+      argument :password, String, required: true
+      argument :password_confirmation, String, required: true
+
     end
-  end
+  
+    def register(**kwargs)
+    
+      user = User.create!(
+        kwargs
+      )
+
+      user 
+
+      rescue ActiveRecord::RecordInvalid => e
+        raise GraphQL::ExecutionError, "Invalid input: #{e.record.errors.full_messages.join(', ')}"
+      end
+
+    end
+  
 end
