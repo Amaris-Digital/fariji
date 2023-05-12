2c0b7f872584b08085d37e4312e763f54b948a12
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-05-12 16:00:22 +0300

    add reset password input fields

---
+++ api/app/graphql/mutations/Inputs/reset_password_input.rb
@@ -0,0 +1,11 @@
+module Types
+    module Inputs
+      class ResetPasswordInput < BaseInputObject
+        argument :phone, String, required: true, description: "User's phone number"
+        argument :otp, String, required: true, description: "One-time password code"
+        argument :new_password, String, required: true, description: "New password"
+        argument :confirm_password, String, required: true, description: "Confirm new password"
+      end
+    end
+  end
+  
