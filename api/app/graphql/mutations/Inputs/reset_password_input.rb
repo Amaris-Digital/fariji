c0e581f3819c8fb3221393e7f9b2b7adc04ca0bf
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-05-12 16:36:12 +0300

    fix uninitialized constant baseInputObject

--- api/app/graphql/mutations/Inputs/reset_password_input.rb
+++ api/app/graphql/mutations/Inputs/reset_password_input.rb
@@ -1,6 +1,6 @@
 module Mutations
     module Inputs
-      class ResetPasswordInput < BaseInputObject
+      class ResetPasswordInput < BaseMutation
         argument :phone, String, required: true, description: "User's phone number"
         argument :otp, String, required: true, description: "One-time password code"
         argument :new_password, String, required: true, description: "New password"
