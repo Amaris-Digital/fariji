d28ba5bc1581ea18678b47478460f5894c6debd1
chelahcynthia <cchelangat989@gmail.com>
2023-05-12 13:42:27 +0300

    Add email and phone validations

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
     has_secure_password
 
     enum :status, { inactive: 0, active: 1 }
-
-    validates :email, uniqueness: true
-    validates :phone, presence: true, length: { minimum: 9, maximum: 12 }
+                                    
+    validates :email, uniqueness: { allow_nil: true }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, allow_nil: true }
+    validates :phone, presence: true, uniqueness: true, length: { minimum: 9, maximum: 12 }
     validates :password, length: { minimum: 6 }, presence: true, format: { with:  /\A(?=.*[A-Z])(?=.*\d).{6,}\z/, message: 'must contain at least one uppercase letter, one number and atleast 6 characters ' }
     validate :at_least_eighteen_years_old
     validates :date_of_birth, presence: true
@@ -12,11 +12,9 @@ class User < ApplicationRecord
     # TODO: add validation for date, which format should be it?
 
     private
-
-    def at_least_eighteen_years_old
+     def at_least_eighteen_years_old
         if self.date_of_birth.present? && self.date_of_birth > 18.years.ago.to_date
             errors.add(:date_of_birth, 'You must be at least eighteen years old')
         end
     end
-
 end
