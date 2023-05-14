d0ebe11fd9baea1fd8f053d2820cfd7bc3f51446
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-14 20:49:21 +0300

    Update encode method with error message

--- api/lib/jwt_service.rb
+++ api/lib/jwt_service.rb
@@ -9,11 +9,6 @@ module TokenAuthorization
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, ENV['SECRET_KEY'])
-  end 
-
-  def self.encode(payload, exp = 24.hours.from_now)
-    payload[:exp] = exp.to_i
     begin
       JWT.encode(payload, ENV['SECRET_KEY'])
     rescue JWT::EncodeError => e
