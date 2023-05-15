9f6394cdc2b6f735b3b801f19177f96fa63e090f
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-05-15 17:54:49 +0300

    fix indentation errors

--- api/lib/app_messaging_service.rb
+++ api/lib/app_messaging_service.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
-module AppMessagingService
+
+module AppMessagingService # rubocop:disable Style/Documentation
   def self.send_otp(phone, otp)
-  client = Twilio::REST::Client.new(ENV.fetch('TWILIO_ACCOUNT_SID'), ENV.fetch('TWILIO_AUTH_TOKEN'))
-      message = client.messages.create(
+    client = Twilio::REST::Client.new(ENV.fetch('TWILIO_ACCOUNT_SID'), ENV.fetch('TWILIO_AUTH_TOKEN'))
+    message = client.messages.create(
       body: "Your OTP is #{otp}",
       from: ENV.fetch('TWILIO_PHONE_NUMBER'),
       to: "+#{phone}"
-      )
-      message.sid
+    )
+    message.sid
   end
 end
