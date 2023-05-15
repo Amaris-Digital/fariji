9553d917d830bfa37c6fd73ec34c85ad362ded4e
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-15 16:24:43 +0300

    add decode and refresh unit test

--- api/spec/lib/jwt_service_spec.rb
+++ api/spec/lib/jwt_service_spec.rb
@@ -15,5 +15,57 @@ RSpec.describe TokenAuthorization do
       expect(decoded_payload).to eq(payload)
     end
   end
+
   
+  describe '.decode' do
+    let(:payload) { { user_id: 123 } }
+    let(:token) { JWT.encode(payload, ENV['SECRET_KEY']) }
+  
+    it 'decodes a JWT token into a payload' do
+      decoded_payload = TokenAuthorization.decode(token)
+      expect(decoded_payload.symbolize_keys).to eq(payload)
+    end
+  end
+
+describe '.refresh' do
+    let(:payload) { { user_id: 123 } }
+    let(:token) { TokenAuthorization.encode(payload, 1.minute.from_now.to_i) }
+  
+    # context 'when the token is valid and not expired' do
+    #     it 'refreshes the token' do
+    #         refreshed_token = TokenAuthorization.refresh(token)
+    #         expect(refreshed_token).not_to be_nil
+          
+    #         decoded_payload = TokenAuthorization.decode(refreshed_token)
+    #         expect(decoded_payload.symbolize_keys).to eq(payload.merge(exp: Time.now.to_i + 1.week.to_i))
+    #       end
+          
+    # end
+  
+    context 'when the token is expired' do
+      let(:token) { TokenAuthorization.encode(payload, 1.minute.ago.to_i) }
+  
+      it 'returns nil' do
+        expect(TokenAuthorization.refresh(token)).to be_nil
+      end
+    end
+  
+    context 'when the token has no expiration time' do
+      let(:token) { TokenAuthorization.encode(payload) }
+  
+      it 'returns nil' do
+        expect(TokenAuthorization.refresh(token)).to be_nil
+      end
+    end
+  
+    context 'when the token is invalid' do
+      let(:token) { 'invalid-token' }
+  
+      it 'returns nil' do
+        expect(TokenAuthorization.refresh(token)).to be_nil
+      end
+    end
+  end
+    
+
   end
