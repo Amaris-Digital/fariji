5230994e8222422ab97a35dd1501b238ad74812c
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-05-20 14:54:20 +0300

    check for new otp record and mark is_valid to true

--- api/app/graphql/mutations/verify_otp.rb
+++ api/app/graphql/mutations/verify_otp.rb
@@ -2,29 +2,31 @@
 
 module Mutations
   class VerifyOtp < BaseMutation
-    argument :otp, String, required: true
+    argument :otp, Integer, required: true
     argument :phone, String, required: true
 
     field :message, String, null: false
     field :status, String, null: false
 
     def resolve(phone:, otp:)
-      # Find the user in the database based on their phone number
       user = User.find_by(phone: phone)
 
       return { status: 'failed', message: 'User not found' } unless user
 
-      otp_record = user.otps.last
-      unless otp_record&.valid? && otp_record.expiry >= Time.zone.now
+      otp_record = Otp.find_by(user: user)
+      puts "Stored OTP: #{otp_record.attributes}"
+      unless otp_record&.is_valid? && otp_record.expiry >= otp_record.created_at
         return { status: 'failed',
                  message: 'OTP has expired or is invalid' }
       end
 
       if otp_record.otp == otp
-        otp_record.update(valid: false) # Mark the OTP as invalid
+        if otp_record.new_record?
+          otp_record.update(is_valid: true)
+        end
         return { status: 'success', message: 'OTP verified successfully' }
       end
-
+    
       { status: 'failed', message: 'Invalid OTP' }
     end
   end
