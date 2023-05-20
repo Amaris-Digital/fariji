d2f4d7de3718851ec3460371e48ccddc5f84ff0c
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-05-20 14:58:25 +0300

    changed column(valid to is_valid) in migration

--- api/db/migrate/20230515101743_create_otps.rb
+++ api/db/migrate/20230515101743_create_otps.rb
@@ -4,7 +4,7 @@ class CreateOtps < ActiveRecord::Migration[7.0]
       t.references :user, null: false, foreign_key: true
       t.integer :otp
       t.datetime :expiry
-      t.boolean :valid
+      t.boolean :is_valid
 
       t.timestamps
     end
