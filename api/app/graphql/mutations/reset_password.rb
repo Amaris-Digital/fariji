49b44fbc66e2b9aa77804f59391143ce84cf42bd
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-05-20 14:51:56 +0300

    change otp field to be integer

--- api/app/graphql/mutations/reset_password.rb
+++ api/app/graphql/mutations/reset_password.rb
@@ -2,28 +2,34 @@
 
 module Mutations
   class ResetPassword < BaseMutation
-    argument :email, String, required: true
-    argument :otp, String, required: true
+    argument :otp, Integer, required: true
     argument :password, String, required: true
     argument :phone, String, required: true
 
     field :message, String, null: false
     field :status, String, null: false
 
-    def resolve(phone:, email:, otp:, password:)
-      user = User.where(phone: phone).or(User.where(email: email))
+    def resolve(phone:, otp:, password:)
+      user = User.find_by(phone: phone)
 
       return { status: 'failed', message: 'User not found' } unless user
+         
+      otp_record = Otp.find_by(user: user)
+      if otp_record.nil?
+        return { status: 'failed', message: 'OTP not found' }
+      end
 
-      otp_record = user.otps.last
-      unless otp_record&.valid? && otp_record.expiry >= Time.zone.now
+      unless otp_record&.is_valid? && otp_record.expiry >= otp_record.created_at
         return { status: 'failed',
                  message: 'OTP has expired or is invalid' }
       end
-
+      
       if otp_record.otp == otp
-        otp_record.update(valid: false)  # Mark the OTP as invalid
-        user.update(password: password)
+        if otp_record.new_record?
+          otp_record.update(is_valid: true)
+        end
+        user.update(password: 'Password111')
+        otp_record.update(is_valid: false)
         return { status: 'success', message: 'Password reset successfully' }
       end
 
