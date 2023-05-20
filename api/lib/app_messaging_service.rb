83789e2d0fa002a27f8d27b3cf4b270022d680c1
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-05-20 14:57:09 +0300

    receive user from sendOtp mutation and set (to) value to be user.phone

--- api/lib/app_messaging_service.rb
+++ api/lib/app_messaging_service.rb
@@ -2,12 +2,12 @@
 
 # This is a reusable module that handles sending out of otp codes via sms
 module AppMessagingService
-  def self.send_otp(phone, otp)
+  def self.send_otp(user, otp)
     client = Twilio::REST::Client.new(ENV.fetch('TWILIO_ACCOUNT_SID'), ENV.fetch('TWILIO_AUTH_TOKEN'))
     message = client.messages.create(
       body: "Your OTP is #{otp}",
       from: ENV.fetch('TWILIO_PHONE_NUMBER'),
-      to: "+#{phone}"
+      to: "+#{user.phone}"
     )
     message.sid
   end
