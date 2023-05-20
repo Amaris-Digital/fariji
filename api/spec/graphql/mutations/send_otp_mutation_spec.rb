880b1abab61bcb8285b8c8532b9f2da77d57353f
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-05-20 14:58:51 +0300

    add sendOtp mutation test

---
+++ api/spec/graphql/mutations/send_otp_mutation_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+require_relative '../../../lib/app_messaging_service'
+RSpec.describe Mutations::SendOtp, type: :request do
+
+    let!(:user) { User.create!(
+      phone: '+254722280617',
+      email: 'johndoe@gmail.com',
+      password: 'Password1',
+      date_of_birth: '1990-01-01',
+      isMuslim: 'true'
+    )}
+
+  
+
+    before do
+      @phone_number = user.phone
+    end
+
+  describe 'sendOtp' do
+    context 'send otp' do
+      
+      before do
+        post '/graphql', params: { query: mutation(phone: @phone_number) }
+      end
+
+      
+      it 'sends the OTP successfully' do
+        puts "user #{user}"
+        
+        expect(response).to have_http_status(200)
+        puts response.body
+        json_response = JSON.parse(response.body)
+        puts json_response
+        data = json_response['data']['sendOtp']
+        
+        expect(data['status']).to eq('success')
+        expect(data['message']).to eq('OTP sent successfully')
+      end
+      
+      it 'creates an OTP record' do
+        expect(Otp.count).to eq(1)
+      end
+      
+    end
+  end
+
+  def mutation(phone: )
+    <<-gql
+      mutation {
+        sendOtp(
+          phone: "#{phone}"
+        ) {
+          status
+          message
+        }
+      }
+    gql
+  end
+ 
+end
