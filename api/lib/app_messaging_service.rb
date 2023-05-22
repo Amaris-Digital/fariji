61fa8a4be64ffeb674cf15781a6fe76b90009b39
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-05-22 08:54:13 +0300

    Add twilio account details

--- api/lib/app_messaging_service.rb
+++ api/lib/app_messaging_service.rb
@@ -2,11 +2,11 @@
 
 # This is a reusable module that handles sending out of otp codes via sms
 module AppMessagingService
-  def self.send_otp(user, otp, account_sid, auth_token, phone_number)
-    client = Twilio::REST::Client.new(ENV.fetch(account_sid, auth_token))
+  def self.send_otp(user, otp)
+    client = Twilio::REST::Client.new(ENV.fetch('TWILIO_ACCOUNT_SID'), ENV.fetch('TWILIO_AUTH_TOKEN'))
     message = client.messages.create(
       body: "Your OTP is #{otp}",
-      from: phone_number,
+      from: ENV.fetch('TWILIO_PHONE_NUMBER'),
       to: "+#{user.phone}"
     )
     message.sid
