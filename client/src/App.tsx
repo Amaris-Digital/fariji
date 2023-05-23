731dfe8e8ea6595c45b45b7f1415fa2cf296c77d
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-05-23 04:59:24 +0300

    Created Registration Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
+import Registration from "./pages/Registration";
 import {Route, Routes} from "react-router-dom";
 
 function App(): JSX.Element {
   return (
     <div>
       <Routes>
-          <Route path={"/"} element={ <Home/> } />
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/registration"} element={<Registration/>} />
       </Routes>
     </div>
   )
