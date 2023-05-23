3dfa0f1e3a8e56b30394143c7e2fbf55eede6ebc
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-05-23 08:59:44 +0300

    Added page 4

--- client/src/pages/Registration.tsx
+++ client/src/pages/Registration.tsx
@@ -4,7 +4,7 @@ import 'swiper/css'
 import 'swiper/css/autoplay'
 import 'swiper/css/scrollbar'
 import { Link } from 'react-router-dom'
-
+import uploadimage from '../assets/uploadimage.svg'
 import { Pagination, Scrollbar } from 'swiper'
 
 const Registration = () => {
@@ -135,8 +135,34 @@ const Registration = () => {
   )
 
   const signUpThree = (
-    <div className='flex flex-col  h-screen'>
-      <p>Already have 3</p>
+    <div className='flex flex-col h-[100vh] justify-between'>
+      <div className='flex justify-between p-4'>
+        <div className='flex gap-1'>
+          Step 3<span className='text-[#626262]'>of 4</span>
+        </div>
+        <div>Exit</div>
+      </div>
+
+      <div className='justify-center pb-24 flex gap-2 flex-col text-center items-center'>
+        <p className='w-[298px] text-3xl font-bold'>Add a Photo</p>
+        <p className='w-[285px] text-center'>Add a photo so other members know who you are</p>
+      </div>
+
+      <div className='justify-center flex gap-2 flex-col text-center items-center h-[139px] w-[139px] mx-auto bg-[#DBDBDB] rounded-full'>
+        <img src={uploadimage} alt='uploadimage' className='w-[56px] h-[56px] flex mx-auto justify-center items-center' />
+      </div>
+
+      <div className='justify-center pb-24 flex gap-4 flex-col text-center items-center'>
+        <button
+          className='w-[312px] h-[41px] text-white bg-[#2A6476]'
+          style={{
+            borderRadius: '8px',
+          }}
+        >
+          Upload Photo
+        </button>
+        <p>Skip</p>
+      </div>
     </div>
   )
 
