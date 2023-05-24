a92f63581475d5a4e8c6ff42da2001117672833c
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-05-24 15:43:00 +0300

    fix CI failing checks

--- api/lib/app_messaging_service.rb
+++ api/lib/app_messaging_service.rb
@@ -3,7 +3,7 @@
 # This is a reusable module that handles sending out of otp codes via sms
 module AppMessagingService
   def self.send_otp(user, otp)
-    client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV.['TWILIO_AUTH_TOKEN'])
+    client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
     message = client.messages.create(
       body: "Your OTP is #{otp}",
       from: ENV['TWILIO_PHONE_NUMBER'],
