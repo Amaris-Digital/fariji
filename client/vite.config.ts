4a65a3beb76b1c13217b81d4fab8388d4226be18
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-24 17:12:48 +0300

    Add PWA Vite plugin configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Fariji',
+        short_name: 'Fariji',
+        theme_color: '#FF9549',
+        background_color: '#ffffff',
+        display: "standalone",
+        start_url: "/"
+      },
+      workbox: {
+         // Cache the index.html file
+        runtimeCaching: [
+          {
+            urlPattern: new RegExp('^https://example.com/$'),
+            handler: 'StaleWhileRevalidate',
+          },
+        ],
+      },
+      registerType: 'autoUpdate',
+    }),
+    
+  ],
 })
