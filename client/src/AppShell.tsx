64e4c0352750304da3735140c6ebd8618b11c0ea
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-05-24 22:43:03 +0300

    add the app shell

---
+++ client/src/AppShell.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AppShell: React.FC = ( { children } ) => {
+  return (
+    <div>
+      <header>
+        <h1>Fariji</h1>
+      </header>
+    </div>
+  );
+};
+
+export default AppShell;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import AppShell from './AppShell'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
@@ -7,7 +8,9 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
       <BrowserRouter>
+        <AppShell >
             <App />
+        </AppShell>
       </BrowserRouter>
   </React.StrictMode>,
 )
