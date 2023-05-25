63ddd1143d99dfaed3568f55cd08f1781db31386
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-25 06:50:02 +0300

    create an android large component

---
+++ client/src/components/AndroidLarge1.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const AndroidLarge1: React.FC = () => {
+  return (
+    <div
+      className="relative w-[360px] h-[800px] bg-[#FF9549]"
+    >
+      <div
+        className="absolute w-[165.74px] h-[52.28px] left-[104px] top-[374px]"
+      >
+        
+        <div className="absolute w-[52px] h-[37.85px] left-0 top-0">
+        <img
+            src="https://myretrobucket.s3.eu-north-1.amazonaws.com/icons8-heart-64.png" 
+            alt="Heart Icon"
+            className="w-full h-full"
+          />
+        </div>
+
+        {/* Title */}
+        <div className="absolute w-[102.74px] h-[52.28px] left-[167px] top-0">
+          Fariji
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AndroidLarge1;
