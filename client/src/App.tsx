86b1fd691b86a10d6e08c8ba8010effaefa02cae
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-25 06:51:00 +0300

    add an android large component and route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import Splash1 from "./components/Splash1";
 import Splash2 from "./components/Splash2";
 import Splash3 from "./components/Splash3";
+import AndroidLarge1 from "./components/AndroidLarge1";
 import {Route, Routes} from "react-router-dom";
 
 function App(): JSX.Element {
@@ -9,6 +10,7 @@ function App(): JSX.Element {
     <div>
       <Routes>
           <Route path={"/"} element={ <Home/> } />
+          <Route path={"/AndroidLarge1"} element={ <AndroidLarge1/> } />
           <Route path={"/Splash1"} element={ <Splash1/> } />
           <Route path={"/Splash2"} element={ <Splash2/> } />
           <Route path={"/Splash3"} element={ <Splash3/> } />
