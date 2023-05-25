75efaa1bdd28fa9a0e2fcdd447d9e657fb32959c
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-25 05:26:01 +0300

    delete vitePWA plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,31 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      manifest: {
-        name: 'Fariji',
-        short_name: 'Fariji',
-        theme_color: '#FF9549',
-        background_color: '#ffffff',
-        display: "standalone",
-        start_url: "/"
-      },
-      workbox: {
-         // Cache the index.html file
-        runtimeCaching: [
-          {
-            urlPattern: new RegExp('^https://example.com/$'),
-            handler: 'StaleWhileRevalidate',
-          },
-        ],
-      },
-      registerType: 'autoUpdate',
-    }),
     
   ],
 })
