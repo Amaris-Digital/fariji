cbe04646b548a3a216d2d96bfafeb9f73ac9ab65
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-25 05:35:06 +0300

    Add Splash2 component and route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import Home from "./pages/Home";
 import Splash1 from "./components/Splash1";
+import Splash2 from "./components/Splash2";
 import {Route, Routes} from "react-router-dom";
 
 function App(): JSX.Element {
@@ -8,6 +9,7 @@ function App(): JSX.Element {
       <Routes>
           <Route path={"/"} element={ <Home/> } />
           <Route path={"/Splash1"} element={ <Splash1/> } />
+          <Route path={"/Splash2"} element={ <Splash2/> } />
       </Routes>
     </div>
   )
