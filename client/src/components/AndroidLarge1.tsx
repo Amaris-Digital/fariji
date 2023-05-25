447474fbaea6e3ee2fa7b278d7660fb94abfd752
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-25 07:15:33 +0300

    Update code to align the icon

--- client/src/components/AndroidLarge1.tsx
+++ client/src/components/AndroidLarge1.tsx
@@ -9,16 +9,16 @@ const AndroidLarge1: React.FC = () => {
         className="absolute w-[165.74px] h-[52.28px] left-[104px] top-[374px]"
       >
         
-        <div className="absolute w-[52px] h-[37.85px] left-0 top-0">
+        <div className="absolute  left-0 top-[20px]">
         <img
             src="https://myretrobucket.s3.eu-north-1.amazonaws.com/icons8-heart-64.png" 
             alt="Heart Icon"
-            className="w-full h-full"
+            className="w-[52px] h-[37.85px]"
           />
         </div>
 
         {/* Title */}
-        <div className="absolute w-[102.74px] h-[52.28px] left-[167px] top-0">
+        <div className="font-heading absolute w-[102.74px] h-[52.28px] left-[63px] top-0 text-[50px] text-white">
           Fariji
         </div>
       </div>
