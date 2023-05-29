be458bcccba2b7f63052891207d371583532aa09
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-29 13:12:14 +0300

    Create splash1.test.ts and add test for Splash1 component

---
+++ client/src/__tests__/splash1.test.ts
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Splash1, { Splash1Props } from '../components/Splash1';
+
+describe('Splash1', () => {
+  const mockOnNext = jest.fn();
+
+  const defaultProps: Splash1Props = {
+    onNext: mockOnNext,
+  };
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should call onNext when Next button is clicked', () => {
+    const { getByText } = render(React.createElement(Splash1, defaultProps));
+    const nextButton = getByText('Next');
+    fireEvent.click(nextButton);
+    expect(mockOnNext).toHaveBeenCalled();
+  });
+});
