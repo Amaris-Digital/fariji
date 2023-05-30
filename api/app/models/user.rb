01a4cd288fd673a8d160202d41749772e6dba441
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-05-30 08:23:57 +0300

    create an attachment to user model and add avatar validations

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_one_attached :avatar
 
     
 
@@ -12,13 +13,29 @@ class User < ApplicationRecord
     validates :password, length: { minimum: 6 }, presence: true, format: { with:  /\A(?=.*[A-Z])(?=.*\d).{6,}\z/, message: 'must contain at least one uppercase letter, one number and atleast 6 characters ' }
     validate :at_least_eighteen_years_old
     validates :date_of_birth, presence: true
+    validate :avatar_validations, if: :avatar_attached?
 
     # TODO: add validation for date, which format should be it?
 
+    
     private
-     def at_least_eighteen_years_old
-        if self.date_of_birth.present? && self.date_of_birth > 18.years.ago.to_date
-            errors.add(:date_of_birth, 'You must be at least eighteen years old')
-        end
+    def at_least_eighteen_years_old
+      if self.date_of_birth.present? && self.date_of_birth > 18.years.ago.to_date
+        errors.add(:date_of_birth, 'You must be at least eighteen years old')
+      end
     end
+
+    def avatar_attached?
+      avatar.attached?
+    end
+
+    def avatar_validations
+      if avatar.attached?
+        unless ['image/png', 'image/jpeg'].include?(avatar.blob.content_type)
+          errors.add(:avatar, 'must be a PNG or JPEG image')
+        end
+      end
+    end
+    
+
 end
