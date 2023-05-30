8f1bdd13b45ddd67194b3dcfa039a8f8b0a6a342
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-30 20:15:30 +0300

    Change the Splash screens importation and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
-import AndroidLarge1 from './components/AndroidLarge1';
-import Splash1 from './components/Splash1';
-import Splash2 from './components/Splash2';
-import Splash3 from './components/Splash3';
+import { Splash1, Splash2, Splash3, Splash4  } from './splash/Splash';
 
 const App: React.FC = () => {
   const [isAppInstalled, setIsAppInstalled] = useState(false);
@@ -29,7 +26,7 @@ const App: React.FC = () => {
   useEffect(() => {
     if (isAppInstalled) {
       if (currentSplash !== 1) {
-        navigate('/android-large1');
+        navigate('/');
       }
     }
   }, [isAppInstalled, currentSplash, navigate]);
@@ -38,17 +35,17 @@ const App: React.FC = () => {
     <div>
       <Routes>
         {!isAppInstalled && currentSplash === 1 && (
-          <Route path="/android-large1" element={<AndroidLarge1 />} />
+          <Route path="/" element={<Splash1 />} />
         )}
 
         <Route path="/home" element={<Home />} />
-        <Route path="/android-large1" element={<AndroidLarge1 />} />
+        <Route path="/" element={<Splash1 />} />
 
         {!isAppInstalled && (
           <>
-            <Route path="/splash1" element={<Splash1 onNext={handleNextSplash} />} />
             <Route path="/splash2" element={<Splash2 onNext={handleNextSplash} />} />
             <Route path="/splash3" element={<Splash3 onNext={handleNextSplash} />} />
+            <Route path="/splash4" element={<Splash4 onNext={handleNextSplash} />} />
           </>
         )}
       </Routes>
