339f0ba7478f8cb1541ed3b615b27bd0c84dbb19
chelahcynthia <cchelangat989@gmail.com>
2023-06-04 11:07:31 +0300

    Sets up graphql connection on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,25 @@ import Home from "./pages/Home";
 import Registration from "./pages/Registration";
 import {Route, Routes} from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from} from "@apollo/client";
-import {onError} from "@apollo/client/link/error"
+import { onError } from "@apollo/client/link/error"
+
+const errorLink = onError(({graphqlErrors, networkError}) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({message, location, path}) => {
+      alert(`Graphql erro ${message}`);
+    });
+  }
+});
+
+const link = from([
+ errorLink,
+  new HttpLink({ uri: "http://localhost:3000/graphiql" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+})
 
 function App(): JSX.Element {
   return (
