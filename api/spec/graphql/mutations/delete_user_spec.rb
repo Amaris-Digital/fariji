1b705345ab715443a1c3ace8c53a1eca3042cdf5
chelahcynthia <cchelangat989@gmail.com>
2023-07-04 13:35:10 +0300

    Creates test that checks http response then confirm user deleteion was successful

---
+++ api/spec/graphql/mutations/delete_user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+Rspec.describe Mutations::DeleteUser, type: :request do
+    describe 'deleteuser' do
+        let!(:user) { create(:user) }
+        let(:mutation) do
+            <<~GQL
+            mutation($id: ID!) {
+              deleteUser(id: $id)
+            }
+          GQL
+        end
+
+        it 'deletes a  user account' do
+            user_id = user.id
+
+            expect do
+                post 'graphql', params: { query: mutation, variables: {id: user_id} }
+            end.to change { User.count }.by(-1)
+
+            expect(response).to have_http_status(:success)
+            json_response = JSON.parse(response.body)
+            expect(json_response['data']['deleteUser']).to eq(true)
+        end 
