c4b60b1ec9e19ab2b98c79197e32753af6b3497b
chelahcynthia <cchelangat989@gmail.com>
2023-07-04 13:37:57 +0300

    Creates a function that handles non-existing id

--- api/spec/graphql/mutations/delete_user_spec.rb
+++ api/spec/graphql/mutations/delete_user_spec.rb
@@ -15,10 +15,16 @@ Rspec.describe Mutations::DeleteUser, type: :request do
             user_id = user.id
 
             expect do
-                post 'graphql', params: { query: mutation, variables: {id: user_id} }
+                post '/graphql', params: { query: mutation, variables: {id: user_id} }
             end.to change { User.count }.by(-1)
 
             expect(response).to have_http_status(:success)
             json_response = JSON.parse(response.body)
             expect(json_response['data']['deleteUser']).to eq(true)
         end 
+
+        it 'returns false if the user account does not exist' do
+            expect do
+                post '/graphql', params: { query: mutation, variables: { id: 'non-existing_id'}}
+            end
+        end
